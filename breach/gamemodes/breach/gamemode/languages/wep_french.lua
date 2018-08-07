89c2a551208e3d445a7bd27c7f53da9a7e379eb7
Domi4nex <42190034+Domi4nex@users.noreply.github.com>
2018-08-08 01:07:28 +0200

    Update wep_french.lua

--- breach/gamemodes/breach/gamemode/languages/wep_french.lua
+++ breach/gamemodes/breach/gamemode/languages/wep_french.lua
@@ -1,138 +1,140 @@
 lang = {}
 
 lang.SCP_0082 = {
-	author = "Kanade, edited by danx91",
-	contact = "Look at this gamemode in workshop and search for creators",
-	purpose = "Infect",
-	instructions = "LMB to attack",
+	author = "Kanade, édité par danx91",
+	contact = "Regardez ce mode de jeu en atelier et recherchez des créateurs",
+	purpose = "Infecter",
+	instructions = "LMB pour attaquer",
 }
 
 lang.SCP_023 = {
 	author = "danx91",
-	contact = "Look at this gamemode in workshop and search for creators",
-	purpose = "Kill",
-	instructions = "LMB to attack",
+	contact = "Regardez ce mode de jeu en atelier et recherchez des créateurs",
+	purpose = "Tuer
+",
+	instructions = "LMB pour attaquer",
 	HUD = {
-		lowhealth = "Your health is too low",
+		lowhealth = "Votre santé est trop faible",
 	},
 }
 
 lang.SCP_049 = {
 	author = "Kanade",
-	contact = "Look at this gamemode in workshop and search for creators",
-	purpose = "'Cure'",
-	instructions = "LMB to 'cure' someone",
+	contact = "Regardez ce mode de jeu en atelier et recherchez des créateurs",
+	purpose = "'Guérir'",
+	instructions = "LMB pour "guérir" quelqu'un",
 	HUD = {
-		attackReady = "Ready to attack",
-		attackCD = "Next attack in",
+		attackReady = "Prêt à attaquer",
+		attackCD = "Prochaine attaque dans",
 	},
 }
 
 lang.SCP_066 = {
 	author = "danx91",
-	contact = "Look at this gamemode in workshop and search for creators",
-	purpose = "Kill",
-	instructions = "LMB to play sound",
+	contact = "Regardez ce mode de jeu en atelier et recherchez des créateurs",
+	purpose = "Tuer",
+	instructions = "LMB joue du son",
 	HUD = {
-		attackReady = "Ready to attack",
+		attackReady = "Prêt à attaquer",
 		attackCD = "Next attack in",
 	},
 }
 
 lang.SCP_082 = {
 	author = "danx91, wepon from M9K Specialities",
-	contact = "Look at this gamemode in workshop and search for creators",
-	purpose = "Kill",
-	instructions = "LMB to attack",
+	contact = "Regardez ce mode de jeu en atelier et recherchez des créateurs",
+	purpose = "Tuer",
+	instructions = "LMB pour attaquer",
 }
 
 lang.SCP_076 = {
 	author = "danx91",
-	contact = "Look at this gamemode in workshop and search for creators",
-	purpose = "Kill",
-	instructions = "LMB to attack",
+	contact = "Regardez ce mode de jeu en atelier et recherchez des créateurs",
+	purpose = "Tuer",
+	instructions = "LMB pour attaquer",
 }
 
 lang.SCP_096 = {
 	author = "Vinrax, edited by danx91",
-	contact = "Look at this gamemode in workshop and search for creators",
-	purpose = "Kill",
-	instructions = "LMB to attack",
+	contact = "Regardez ce mode de jeu en atelier et recherchez des créateurs",
+	purpose = "Tuer",
+	instructions = "LMB pour attaquer",
 }
 
 lang.SCP_106 = {
 	author = "Kanade",
-	contact = "Look at this gamemode in workshop and search for creators",
-	purpose = "Kill",
-	instructions = "LMB to teleport someone to pocket dimension\nRMB to place teleport\nR to teleport yourself to marked position",
+	contact = "Regardez ce mode de jeu en atelier et recherchez des créateurs",
+	purpose = "Tuer",
+	instructions = "LMB pour téléporter quelqu'un à la poche dimension \ nRMB pour placer le téléport \ nR pour vous téléporter à la position marquée",
 	HUD = {
-		attackReady = "Ready to attack",
-		attackCD = "Next attack in",
-		teleportReady = "Teleport ready",
-		teleportCD = "Next teleport in"
+		attackReady = "Prêt à attaquer",
+		attackCD = "Prochaine attaque dans",
+		teleportReady = "Téléportation prête",
+		teleportCD = "Prochain téléport dans"
 	},
 }
 
 lang.SCP_173 = {
 	author = "Kanade",
-	contact = "Look at this gamemode in workshop and search for creators",
-	purpose = "Kill",
-	instructions = "LMB to kill someone, RMB to blind you targets",
+	contact = "Regardez ce mode de jeu en atelier et recherchez des créateurs",
+	purpose = "Tuer",
+	instructions = "LMB pour tuer quelqu'un, RMB pour aveugler vos cibles",
 	HUD = {
-		nlook = "Noone is looking",
-		specCD = "Speial ready to use in",
-		specReady = "Special ready to use",
-		slook = "Someone is looking",
+		nlook = "Personne ne cherche",
+		specCD = "Spécial prêt à l'emploi",
+		specReady = "Spécial prêt à l'emploi",
+		slook = "Quelqu'un cherche",
 	}
 }
 
 lang.SCP_420j = {
 	author = "danx91",
-	contact = "Look at this gamemode in workshop and search for creators",
-	purpose = "The Best weed in the World",
-	instructions = "MAN DATS SUM GOOD ASS SHIT",
+	contact = "Regardez ce mode de jeu en atelier et recherchez des créateurs",
+	purpose = "La meilleure herbe au monde",
+	instructions = "HOMME DATS SUM GOOD ASS SHIT",
 }
 
 lang.SCP_457 = {
 	author = "Kanade",
-	contact = "Look at this gamemode in workshop and search for creators",
-	purpose = "Burn",
+	contact = "Regardez ce mode de jeu en atelier et recherchez des créateurs",
+	purpose = "Brûler
+",
 	instructions = "",
 }
 
 lang.SCP_500 = {
 	author = "danx91",
-	contact = "Look at this gamemode in workshop and search for creators",
-	purpose = "Heal yourself",
-	instructions = "LMB to use, or it will be triggered automatically when you should receive deadly hit",
+	contact = "Regardez ce mode de jeu en atelier et recherchez des créateurs",
+	purpose = "Se soigner",
+	instructions = "LMB à utiliser, ou il sera déclenché automatiquement lorsque vous devriez recevoir un coup mortel",
 }
 
 lang.SCP_682 = {
 	author = "danx91",
-	contact = "Look at this gamemode in workshop and search for creators",
-	purpose = "Kill",
-	instructions = "LMB to attack, RMB to use ability",
+	contact = "Regardez ce mode de jeu en atelier et recherchez des créateurs",
+	purpose = "Tuer",
+	instructions = "LMB pour attaquer, RMB pour utiliser la capacité",
 	HUD = {
-		attackReady = "Ability ready",
-		attackCD = "Ability ready in",
+		attackReady = "Aptitude prête",
+		attackCD = "Capacité prête dans",
 	},
 }
 
 lang.SCP_689 = {
 	author = "danx91",
-	contact = "Look at this gamemode in workshop and search for creators",
-	purpose = "Kill",
-	instructions = "LMB to teleport yourself to your target",
+	contact = "Regardez ce mode de jeu en atelier et recherchez des créateurs",
+	purpose = "Tuer",
+	instructions = "LMB pour vous téléporter vers votre cible",
 	HUD = {
-		attackReady = "Ready to attack",
-		attackCD = "Next attack in",
-		targets = "Targets"
+		attackReady = "Prêt à attaquer",
+		attackCD = "Prochaine attaque dans",
+		targets = "Cibles"
 	},
 }
 
 lang.SCP_714 = {
 	author = "danx91",
-	contact = "Look at this gamemode in workshop and search for creators",
+	contact = "Regardez ce mode de jeu en atelier et recherchez des créateurs",
 	purpose = "Protect yourself",
 	instructions = "Hold to use",
 	HUD = {
@@ -144,14 +146,14 @@ lang.SCP_714 = {
 
 lang.SCP_8602 = {
 	author = "danx91",
-	contact = "Look at this gamemode in workshop and search for creators",
+	contact = "Regardez ce mode de jeu en atelier et recherchez des créateurs",
 	purpose = "Kill",
 	instructions = "LMB to attack",
 }
 
 lang.SCP_939 = {
 	author = "danx91",
-	contact = "Look at this gamemode in workshop and search for creators",
+	contact = "Regardez ce mode de jeu en atelier et recherchez des créateurs",
 	purpose = "Kill",
 	instructions = "LMB to attack, RMB to change voice chat",
 	HUD = {
@@ -163,7 +165,7 @@ lang.SCP_939 = {
 
 lang.SCP_957 = {
 	author = "danx91",
-	contact = "Look at this gamemode in workshop and search for creators",
+	contact = "Regardez ce mode de jeu en atelier et recherchez des créateurs",
 	purpose = "Kill",
 	instructions = "LMB to deal AOE damage",
 	HUD = {
@@ -176,14 +178,14 @@ lang.SCP_957 = {
 
 lang.SCP_966 = {
 	author = "Kanade, edited by danx91",
-	contact = "Look at this gamemode in workshop and search for creators",
+	contact = "Regardez ce mode de jeu en atelier et recherchez des créateurs",
 	purpose = "Kill",
 	instructions = "You deal damage when you are near human",
 }
 
 lang.SCP_999 = {
 	author = "danx91",
-	contact = "Look at this gamemode in workshop and search for creators",
+	contact = "Regardez ce mode de jeu en atelier et recherchez des créateurs",
 	purpose = "Heal everybody",
 	instructions = "LMB to heal target, RMB to heal everybody in close area",
 	HUD = {
@@ -196,7 +198,7 @@ lang.SCP_999 = {
 
 lang.SCP_1048A = {
 	author = "danx91",
-	contact = "Look at this gamemode in workshop and search for creators",
+	contact = "Regardez ce mode de jeu en atelier et recherchez des créateurs",
 	purpose = "Kill",
 	instructions = "LMB to emit loud scream",
 	HUD = {
@@ -207,14 +209,14 @@ lang.SCP_1048A = {
 
 lang.SCP_1048B = {
 	author = "danx91",
-	contact = "Look at this gamemode in workshop and search for creators",
+	contact = "Regardez ce mode de jeu en atelier et recherchez des créateurs",
 	purpose = "Kill",
 	instructions = "LMB to attack",
 }
 
 lang.SCP_1471 = {
 	author = "danx91",
-	contact = "Look at this gamemode in workshop and search for creators",
+	contact = "Regardez ce mode de jeu en atelier et recherchez des créateurs",
 	purpose = "Kill",
 	instructions = "LMB to attack, RMB to teleport yourself",
 	HUD = {
@@ -225,7 +227,7 @@ lang.SCP_1471 = {
 
 lang.SCP_SantaJ = {
 	author = "danx91",
-	contact = "Look at this gamemode in workshop and search for creators",
+	contact = "Regardez ce mode de jeu en atelier et recherchez des créateurs",
 	purpose = "Give gifts",
 	instructions = "LMB to throw gift",
 	HUD = {
@@ -239,7 +241,7 @@ lang.SCP_SantaJ = {
 
 lang.HOLSTER = {
 	author = "Kanade",
-	contact = "Look at this gamemode in workshop and search for creators",
+	contact = "Regardez ce mode de jeu en atelier et recherchez des créateurs",
 	purpose = "Holster",
 	instructions = "",
 }
@@ -255,7 +257,7 @@ lang.ID = {
 
 lang.CV = {
 	author = "Kanade",
-	contact = "Look at this gamemode in workshop and search for creators",
+	contact = "Regardez ce mode de jeu en atelier et recherchez des créateurs",
 	purpose = "Look at cameras",
 	instructions = "If you hold it, click RMB to change the view",
 	changed = "Changed to"
@@ -263,28 +265,28 @@ lang.CV = {
 
 lang.EYEDROPS = {
 	author = "Kanade",
-	contact = "Look at this gamemode in workshop and search for creators",
+	contact = "Regardez ce mode de jeu en atelier et recherchez des créateurs",
 	purpose = "Stops blinking for 10 seconds",
 	instructions = "Click left to use",
 }
 
 lang.MEDKIT = {
 	author = "Kanade",
-	contact = "Look at this gamemode in workshop and search for creators",
+	contact = "Regardez ce mode de jeu en atelier et recherchez des créateurs",
 	purpose = "Heal yourself or other people",
 	instructions = "Click left to heal yourself, right to heal others",
 }
 
 lang.NVG = {
 	author = "Kanade",
-	contact = "Look at this gamemode in workshop and search for creators",
+	contact = "Regardez ce mode de jeu en atelier et recherchez des créateurs",
 	purpose = "Gives you night vision",
 	instructions = "Hold it",
 }
 
 lang.RADIO = {
 	author = "Kanade",
-	contact = "Look at this gamemode in workshop and search for creators",
+	contact = "Regardez ce mode de jeu en atelier et recherchez des créateurs",
 	purpose = "Communicate",
 	instructions = "If you hold it, your chat messages and voice chat will be global with others that have the radio",
 	channel = "Radio Channel",
@@ -293,14 +295,14 @@ lang.RADIO = {
 
 lang.SNAV = {
 	author = "Kanade",
-	contact = "Look at this gamemode in workshop and search for creators",
+	contact = "Regardez ce mode de jeu en atelier et recherchez des créateurs",
 	purpose = "Find a way to go",
 	instructions = "If you hold it, click RMB to change the view",
 }
 
 lang.SNAV_ULT = {
 	author = "Kanade",
-	contact = "Look at this gamemode in workshop and search for creators",
+	contact = "Regardez ce mode de jeu en atelier et recherchez des créateurs",
 	purpose = "Find a way to go and scan for enemies",
 	instructions = "If you hold it, click RMB to change the view",
 	mtfdetect = "MTF Guard detected",
@@ -314,7 +316,7 @@ lang.SNAV_ULT = {
 
 lang.KEYCARD = {
 	author = "danx91",
-	contact = "Look at this gamemode in workshop and search for creators",
+	contact = "Regardez ce mode de jeu en atelier et recherchez des créateurs",
 	purpose = "Opens doors which requires keycard (Hold reload key to see where card have access)",
 	instructions = "Access:",
 	ACC = {
